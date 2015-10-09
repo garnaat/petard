cf7974f9bc7082717361f60f7845ee5ceb370091
Mitch Garnaat <mitch@garnaat.com>
2015-10-09 13:40:14 -0700

    Add include_package_data to tell setup.py to use MANIFEST.in when installing package data.

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
     author_email='mitch@garnaat.com',
     url='https://github.com/garnaat/petard',
     packages=find_packages(exclude=['tests*']),
-    package_data={'petard': ['_version', 'data/*.json', 'data/*/*.json']},
+    include_package_data=True,
     install_requires=requires,
     license=open("LICENSE").read(),
     classifiers=(
